a55f31bdb1f5b3f287bf30ae4562bd09516b8fcb
Satyaveerjaligama <satyaveerjaligama26@gmail.com>
2024-08-06 23:15:21 +0530

    Updated readme file and added images of add to cart fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,25 @@
+import { Typography } from "@mui/material";
 import Link from "next/link";
 
 export default function Home() {
+  const observations = [
+    {
+      label: 'Amazon',
+      href: '/amazon'
+    },
+  ];
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Link href="/amazon">
-        Amazon
-      </Link>
+      <Typography variant="h6">Observations</Typography>
+      {observations.map((singleObservation, index)=>
+      <Typography key={singleObservation.href}>
+        {index + 1}.{' '}
+        <Link href={singleObservation.href}>
+          {singleObservation.label}
+        </Link>
+      </Typography>
+      )}
     </main>
   );
 }
